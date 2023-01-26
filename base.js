7cb0046fca47a115b3305077d60417fc5ac0ed21
houswork <123581710+houswork@users.noreply.github.com>
2023-01-26 14:13:23 +0300

    MathTest

--- base.js
+++ base.js
@@ -1,14 +1,34 @@
+let button = document.getElementById('finish');
 
-const div = document.createElement('div');
-const header = document.createElement('h1');
-header.innerText = 'JavaScript'
-header.classList.add('test')
-const a = document.createElement('a');
-a.href = 'https://vk.com/lailailalalai'
-a.innerText = 'Мой Vk'
+button.addEventListener('click', checkTest);
 
-div.appendChild(header)
-div.appendChild(a)
-document.body.appendChild(div)
-console.log(div)
-console.log(a)  
+function checkTest()  {
+    let reesult = 0;
+    
+    let a1 = document.getElementById('q1').value;
+    if (a1 === '4')  {
+        reesult++;
+    }
+    
+    let a2 = document.getElementById('q2').value;
+    if (a2 === '6')  {
+        reesult++;
+    }
+    
+    let a3 = document.getElementById('q3').value;
+    if (a3 === '4')  {
+        reesult++;
+    }
+    
+    let a4 = document.getElementById('q4').value;
+    if (a4 === '11')  {
+        reesult++;
+    }
+    
+    let a5 = document.getElementById('q5').value;
+    if (a5 === '36')  {
+        reesult++;
+    }
+
+    alert('Количество правильных ответов' + reesult);
+}
